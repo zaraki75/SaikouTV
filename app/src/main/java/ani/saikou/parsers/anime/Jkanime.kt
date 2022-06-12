562fcb68e3a5c22da05554365ad09b79a7f94fdd
Diego Peña Y Lillo <80992641+Diegopyl1209@users.noreply.github.com>
2022-06-12 18:15:52 -0400

    Possible fix to slow loading in animes with much episodes

--- app/src/main/java/ani/saikou/parsers/anime/Jkanime.kt
+++ app/src/main/java/ani/saikou/parsers/anime/Jkanime.kt
@@ -1,14 +1,11 @@
 package ani.saikou.parsers.anime
 
 import android.net.Uri
-import android.util.Base64
 import android.util.Log
 import ani.saikou.*
 import ani.saikou.parsers.*
 import ani.saikou.parsers.anime.extractors.FPlayer
-import ani.saikou.parsers.anime.extractors.GogoCDN
 import ani.saikou.parsers.anime.extractors.OK
-import ani.saikou.parsers.anime.extractors.StreamSB
 import com.fasterxml.jackson.module.kotlin.readValue
 
 class Jkanime : AnimeParser() {
@@ -19,25 +16,51 @@ class Jkanime : AnimeParser() {
     override val language = "Spanish"
 
     override suspend fun loadEpisodes(animeLink: String, extra: Map<String, String>?): List<Episode> {
-        val episodes = mutableListOf<Episode>()
         val pageBody = client.get(animeLink).document
-        val animeId = pageBody.select("div.anime__details__text div.anime__details__title div#guardar-anime.btn.btn-light.btn-sm.ml-2")
-            .attr("data-anime")
-         pageBody.select("div.anime__pagination a").forEach {
-            val pageNum = it.attr("href").replace("#pag","")
-            val json = client.get("https://jkanime.net/ajax/pagination_episodes/$animeId/$pageNum").parsed<List<ResponseElement>>()
-            json.forEach(){  result ->
-                episodes.add(
-                    if(result.image.isNotBlank()){
-                        Episode(result.number,"$animeLink/${result.number}",thumbnail = "https://cdn.jkdesu.com/assets/images/animes/video/image_thumb/${result.image}")
-                    }else{
-                        Episode(result.number,"$animeLink/${result.number}")
+        val animeId =
+            pageBody.select("div.anime__details__text div.anime__details__title div#guardar-anime.btn.btn-light.btn-sm.ml-2")
+                .attr("data-anime")
+        return episodesParser(animeLink,animeId)
+        }
+
+    private suspend fun episodesParser(animeLink: String, animeId:String):List<Episode>{
+        val pageBody = client.get(animeLink).document
+        val episodes = mutableListOf<Episode>()
+        val lastPage = pageBody.select("div.anime__pagination a").last()?.attr("href")
+            ?.replace("#pag","")
+        val firstPage = pageBody.select("div.anime__pagination a").first()?.attr("href")
+            ?.replace("#pag","")
+
+
+        if(firstPage != lastPage) {
+                var checkLast = 0;
+                for (i in 1 until lastPage?.toInt()!!) {
+                    for (j in 1..12){
+                        Log.i("bruh",(j + checkLast).toString())
+                        episodes.add(
+                            Episode(
+                                (j + checkLast).toString(),
+                                "$animeLink/${j + checkLast}"
+                            )
+                        )
+
                     }
-                )
+                    checkLast += 12
+                }
+            client.get("https://jkanime.net/ajax/pagination_episodes/$animeId/$lastPage").parsed<List<ResponseElement>>().forEach{
+                episodes.add(Episode(it.number,"$animeLink/${it.number}"))
             }
         }
-    return episodes
-    }
+        if(firstPage == lastPage){
+            client.get("https://jkanime.net/ajax/pagination_episodes/$animeId/$lastPage").parsed<List<ResponseElement>>().forEach{
+                Log.i("bruh",it.number)
+                episodes.add(Episode(it.number,"$animeLink/${it.number}"))
+            }
+        }
+
+        return episodes
+        }
+
 
     data class ResponseElement(
         val number: String,
